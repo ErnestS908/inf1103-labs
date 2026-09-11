4aa70000609ce565d7bc16f4efcfc4897c29c943
ErnestS908 <2601691@sit.singaporetech.edu.sg>
2026-09-11 21:46:49 +0800

    First, Second and Third

---
+++ auditor.py
@@ -0,0 +1,16 @@
+inventory = 0
+while 1:
+    user_input=input("Enter stock quantity or type 'quit' to exit: ")
+    
+    if user_input == "quit" :
+        print("Ending program")
+        break
+
+    else:
+        stock = int(user_input)
+  
+
+
+
+
+
