a405c016ccffbf112b27a01d9d54e76b8a2403d4
ErnestS908 <2601691@sit.singaporetech.edu.sg>
2026-09-11 22:00:16 +0800

    Eight

--- auditor.py
+++ auditor.py
@@ -1,21 +1,24 @@
 inventory = 0
+rejected = 0
 while 1:
     user_input=input("Enter stock quantity or type 'quit' to exit: ")
     
     if user_input == "quit" :
         print("Ending program")
+        print("Total number of rejected errors this cycle: ", rejected)
+        print("Total units processed: ", inventory)
         break
 
     elif user_input.isdigit() == 1:
         stock = int(user_input)
         inventory += stock
-        print("Current stock: ",inventory )
         if inventory>500:
             print("Inventory exceeds 500")
             break
 
     elif user_input.isdigit() == 0:
         print("Error please enter a non-negative integer")
+        rejected += 1
   
 
 
